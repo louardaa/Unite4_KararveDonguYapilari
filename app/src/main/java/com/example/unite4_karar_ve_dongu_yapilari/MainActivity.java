9caa4d03694c02431080307f30029c636da30244
BIL-LAB2PC00\Ogrenci <seyydinburhanedin.mtal38@gmail.com>
2022-12-14 11:08:11 +0300

    Ünite 4 Projemiz Oluşturuldu

---
+++ app/src/main/java/com/example/unite4_karar_ve_dongu_yapilari/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.unite4_karar_ve_dongu_yapilari;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
