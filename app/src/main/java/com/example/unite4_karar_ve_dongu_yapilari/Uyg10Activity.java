3789b24949843e0d3bcf83b28f9300193cd75086
BIL-LAB2PC00\Ogrenci <seyydinburhanedin.mtal38@gmail.com>
2023-01-11 11:05:19 +0300

    no message

--- app/src/main/java/com/example/unite4_karar_ve_dongu_yapilari/Uyg10Activity.java
+++ app/src/main/java/com/example/unite4_karar_ve_dongu_yapilari/Uyg10Activity.java
@@ -1,4 +1,33 @@
 package com.example.unite4_karar_ve_dongu_yapilari;
 
-public class Uyg10Activity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Uyg10Activity extends AppCompatActivity {
+    EditText number;
+    Button button;
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.uyg10_activity);
+        number = findViewById(R.id.txtSayi);
+        button = findViewById(R.id.btnOnayla);
+        textView = findViewById(R.id.textView2);
+    }
+    public void btnOnayla (View view) {
+        int sayi = Integer.parseInt(number.getText().toString());
+        int sayac=1;
+        long sonuc=1;
+        while(sayac<=sayi){
+            sonuc=sonuc*sayac;
+            sayac++;
+        }
+        textView.setText("Sonuç: "+sonuc);
+    }
 }
